f3c3d211798f435d95ff0f02450f3d5f2838f609
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2019-10-25 18:55:56 +0200

    Added the detector - acquired on both the 1M and 100k, the latter at 200 Hz with zero delay.

---
+++ src/pilatusproxy/Pilatus.py
@@ -0,0 +1,100 @@
+import re
+import time
+import socket
+import select
+import zmq
+
+class Pilatus:
+    def __init__(self, hostname):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((hostname, 41234))
+        self.context = zmq.Context()
+        self.req_socket = self.context.socket(zmq.REQ)
+        self.req_socket.connect('tcp://%s:9998' %hostname)
+        self.nimages = 1
+        
+    def parse_response(self, pattern, timeout):
+        ready = select.select([self.sock], [], [], timeout)
+        if ready[0]:
+            response = self.sock.recv(1024)
+            #print(response)
+            match = re.compile(pattern).match(response)
+            ret = match.groups(1)[0] if match else None
+            #print(ret)
+            return ret
+
+    def check_response(self, pattern, timeout):
+        ready = select.select([self.sock], [], [], timeout)
+        if ready[0]:
+            response = self.sock.recv(1024)
+            #print(response)
+            ret = True if response.startswith(pattern) else False
+            return ret
+
+    def get_nimages(self):
+         self.sock.send(b'nimages\0')
+         response = self.parse_response('15 OK N images set to: (\d+)', 1.0)
+         nimages = int(response) if response else 0
+         return nimages
+   
+    def set_nimages(self, value):
+        self.sock.send(b'nimages %d\0' %value)
+        if not self.check_response('15 OK', 1.0):
+            print('Error setting nimages')
+        self.nimages = value
+
+    def get_imgpath(self):
+        self.sock.send(b'imgpath\0')
+        response = self.parse_response('10 OK (.*)\x18', 1.0)
+        return response
+
+    def set_imgpath(self, path):
+        self.sock.send(b'imgpath %s\0' %path)
+        if not self.check_response('10 OK', 1.0):
+            print('Error setting imgpath')
+            
+    def get_exptime(self):
+        self.sock.send(b'exptime\0')
+        response = self.parse_response('15 OK Exposure time set to: (.*) sec.\x18', 1.0)
+        exptime = float(response) if response else 0
+        return exptime
+
+    def set_exptime(self, value):
+        self.sock.send(b'exptime %f\0' %value)
+        if not self.check_response('15 OK', 1.0):
+            print('Error setting imgpath')
+            
+    def get_expperiod(self):
+        self.sock.send(b'expperiod\0')
+        response = self.parse_response('15 OK Exposure period set to: (.*) sec\x18', 1.0)
+        exptime = float(response) if response else 0
+        return exptime
+
+    def set_expperiod(self, value):
+        self.sock.send(b'expperiod %f\0' %value)
+        if not self.check_response('15 OK', 1.0):
+            print('Error setting imgpath')
+            
+    def exposure(self, filename):
+        self.req_socket.send_string(str(self.nimages))
+        print('send nimages')
+        print(self.req_socket.recv())
+        
+        self.sock.send(b'exposure %s\0' %filename)
+        if not self.check_response('15 OK  Starting', 1.0):
+            print('Error starting exposure')
+        #response = self.sock.recv(1024)
+        #print(response)
+        
+    def acquiring(self):
+        ready = select.select([self.sock], [], [])
+        if ready[0]:
+            response = self.sock.recv(1024)
+            if response.startswith('7 OK'):
+                return False
+            else:
+                print('Unkown response: %s' %response)
+                return True
+        else:
+            return True
+
--- src/pilatusproxy/pilatusproxy.py
+++ src/pilatusproxy/pilatusproxy.py
@@ -1,75 +1,68 @@
 # Tango imports
 from tango import DevState
 from tango.server import Device, attribute, command, server_run,  device_property
-
+from .Pilatus import Pilatus
 
 class PilatusProxyDS(Device):
-    """ A Pilatus device
-
-       Device States Description:
-    #
-    #   DevState.ON :     The device is in operation
-    #   DevState.INIT :   Initialisation of the communication with the device and initial configuration
-    #   DevState.FAULT :  The Device has a problem and cannot handle further requests.
-    #   DevState.MOVING : The Device is engaged in an acquisition.
+    """
+    A Pilatus device
     """
 
     ### Properties ###
 
-    Host = device_property(dtype=str, default_value="b-nanomax-mobile-ipc-01", doc="hostname")
-    Port = device_property(dtype=int, default_value=41234)
+    Host = device_property(dtype=str, default_value="b-nanomax-pilatus1m-ipc-01", doc="hostname")
 
     ### Attributes ###
 
     @attribute(label='nimages', dtype=int, doc='Number of images')
     def nimages(self):
-        print('getting nimages')
-        return 123
+        return self.det.get_nimages()
 
     @nimages.write
     def nimages(self, val):
-        print('setting nimages to %u' % val)
+        self.det.set_nimages(val)
 
     @attribute(label='imgpath', dtype=str, doc='Local DCU image path')
     def imgpath(self):
-        print('getting imgpath')
-        return '/some/path'
+        return self.det.get_imgpath()
 
     @imgpath.write
     def imgpath(self, val):
-        print('setting imgpath to %s' % val)
+        self.det.set_imgpath(val)
 
     @attribute(label='exptime', dtype=float, doc='Exposure time', unit='s')
     def exptime(self):
-        print('getting exptime')
-        return .007
+        return self.det.get_exptime()
 
     @exptime.write
     def exptime(self, val):
-        print('setting exptime to %f' % val)
+        self.det.set_exptime(val)
 
     @attribute(label='expperiod', dtype=float, doc='Exposure period (exposure time + readout)', unit='s')
     def expperiod(self):
-        print('getting expperiod')
-        return .010
+        return self.det.get_expperiod()
 
     @expperiod.write
     def expperiod(self, val):
-        print('setting expperiod to %f' % val)
+        self.det.set_expperiod(val)
 
-    ### Commands ###
+    ### Commands ###nimages
     
     @command(dtype_in=str)
     def exposure(self, filename):
-        print('Software triggered exposure to %s' % filename)
+        print 'exposure()'
+        self.set_state(DevState.RUNNING)
+        if not filename.endswith('.tif'):
+            filename += '.tif'
+        self.det.exposure(filename)
 
     @command(dtype_in=str)
     def exttrigger(self, filename):
-        print('Hardware triggered exposure to %s (one trigger for the whole train)' % filename)
+        raise NotImplementedError('Hardware triggered exposure to %s (one trigger for the whole train)' % filename)
 
     @command(dtype_in=str)
     def extmtrigger(self, filename):
-        print('Hardware triggered exposure to %s (one trigger per frame)' % filename)
+        raise NotImplementedError('Hardware triggered exposure to %s (one trigger per frame)' % filename)
 
 
     ### Other stuff ###
@@ -77,20 +70,27 @@ class PilatusProxyDS(Device):
     # Device methods
     def init_device(self):
         """Instantiate device object, do initial instrument configuration."""
+        print 'init_device()'
         self.set_state(DevState.INIT)
         
         try:
             self.get_device_properties()
-        
         except Exception as e:
             self.set_state(DevState.FAULT)
             self.set_status('Device failed at init: %s' % e.message)
+
+        print 'going to connect to %s' % self.Host
+        self.det = Pilatus(self.Host)
         self.set_state(DevState.ON)
 
     # This sets the state before every command
     def always_executed_hook(self):
-        print('Here we should update the state, is the pilatus ON, MOVING, FAULT, or INIT?')
+        print 'always_executed_hook()'
 
+        if self.get_state() == DevState.RUNNING:
+            if not self.det.acquiring():
+                self.set_state(DevState.ON)
+                print '...was not running'
 
 def main():
     server_run((PilatusProxyDS,))
